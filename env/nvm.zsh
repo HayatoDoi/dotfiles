e160f644dd358d96e33b1ebb414ffe409124a497
HayatoDoi <b1517914@planet.kanazawa-it.ac.jp>
2018-11-30 10:32:36 +0900

    edit nvm config

--- env/nvm.zsh
+++ env/nvm.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 # nvm
-export NVM_DIR=${HOME}"/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-export NODE_PATH=$(npm root -g)
+[ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
+source /usr/share/nvm/nvm.sh
+source /usr/share/nvm/bash_completion
+source /usr/share/nvm/install-nvm-exec
