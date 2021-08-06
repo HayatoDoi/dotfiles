a1490667468484af673ec3b3a3d18ae6d33ba568
HayatoDoi <hayato.doi@mail.exploit.moe>
2021-08-06 12:25:28 +0900

    update backup flow

---
+++ zshrc/buildin_cmd/rm.zsh
@@ -0,0 +1,20 @@
+function rm() {
+	TRASH_DIR='/var/tmp/trash'
+	RM='rm'
+	MV='mv'
+	MKDIR='mkdir'
+
+	echo $* | while read target; do
+		full_path=`readlink -f $target`
+		backup=${TRASH_DIR}/${full_path}
+		backup_dir=${TRASH_DIR}`dirname $full_path`
+
+		if [ -e ${backup} ]; then
+			# 1世代のみバックアップ
+			command ${RM} -rf ${backup}
+		fi
+		command ${MKDIR} -p ${backup_dir}
+		command ${MV} ${target} ${backup}
+	done
+}
+
