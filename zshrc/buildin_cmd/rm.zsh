5bc31a28e9488484677e286921c13a46c01208e9
HayatoDoi <hayato.doi@mail.exploit.moe>
2023-12-20 10:46:45 +0900

    特定環境で発生する実行エラーを修正

--- zshrc/buildin_cmd/rm.zsh
+++ zshrc/buildin_cmd/rm.zsh
@@ -4,17 +4,16 @@ function rm() {
 	MV='mv'
 	MKDIR='mkdir -p'
 
-	$MKDIR $TRASH_DIR
+	eval command ${MKDIR} ${TRASH_DIR}
 	echo $* | while read target; do
 		full_path=`readlink -f $target`
 		backup=${TRASH_DIR}/${full_path}
 		backup_dir=${TRASH_DIR}`dirname $full_path`
-
 		if [ -e ${backup} ]; then
 			# 1世代のみバックアップ
-			command ${RM} -rf ${backup}
+			eval command ${RM} -rf ${backup}
 		fi
-		command ${MKDIR} ${backup_dir}
-		command ${MV} ${target} ${backup}
+		eval command ${MKDIR} ${backup_dir}
+		eval command ${MV} ${target} ${backup}
 	done
 }
